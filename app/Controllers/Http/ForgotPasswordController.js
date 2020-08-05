3a01323984d4e66b31490069963f2a38c0caa94b
Lucas Teles <dev.teles@outlook.com>
2020-08-05 14:21:05 -0300

    Add reset password

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,3 +1,4 @@
+const moment = require('moment')
 const crypto = require('crypto')
 
 const User = use('App/Models/User')
@@ -33,6 +34,32 @@ class ForgotPasswordController {
       return response
         .status(error.status)
         .send({ error: { message: 'Unable to recover password' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response
+          .send({ error: { message: 'The token is expired' } })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (error) {
+      return response
+        .send({ error: { message: 'Could not recover password' } })
     }
   }
 }
