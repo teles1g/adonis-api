87e07e31ce2422707ff7a259ca79bafea58a1a7e
Lucas Teles <dev.teles@outlook.com>
2020-08-16 22:11:50 -0300

    List project

--- app/Controllers/Http/ProjectController.js
+++ app/Controllers/Http/ProjectController.js
@@ -1,6 +1,14 @@
 const Project = use('App/Models/Project')
 
 class ProjectController {
+  async index ({ request, response, view }) {
+    const projects = await Project.query()
+      .with('user')
+      .fetch()
+
+    return projects
+  }
+
   async store ({ request, response, auth }) {
     const data = request.only(['title', 'description'])
 
