ab9b9e9be27427a2393d05465fc06e827c9988e1
Lucas Teles <dev.teles@outlook.com>
2020-08-18 23:28:51 -0300

    Add reset password validator

--- start/routes.js
+++ start/routes.js
@@ -18,7 +18,7 @@ Route.post('/users', 'UserController.store').validator('User')
 Route.post('/sessions', 'SessionController.store').validator('Session')
 
 Route.post('/passwords', 'ForgotPasswordController.store').validator('ForgotPassword')
-Route.put('/passwords', 'ForgotPasswordController.update')
+Route.put('/passwords', 'ForgotPasswordController.update').validator('ResetPassword')
 
 Route.get('/files/:id', 'FileController.show')
 
