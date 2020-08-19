a7793469295f129f01bbd32107b09886a3e360dc
Lucas Teles <dev.teles@outlook.com>
2020-08-18 23:28:21 -0300

    Add forgot password validator

--- start/routes.js
+++ start/routes.js
@@ -17,7 +17,7 @@ const Route = use('Route')
 Route.post('/users', 'UserController.store').validator('User')
 Route.post('/sessions', 'SessionController.store').validator('Session')
 
-Route.post('/passwords', 'ForgotPasswordController.store')
+Route.post('/passwords', 'ForgotPasswordController.store').validator('ForgotPassword')
 Route.put('/passwords', 'ForgotPasswordController.update')
 
 Route.get('/files/:id', 'FileController.show')
